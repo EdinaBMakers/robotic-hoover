bdeed5bfe794ec762e5523c599c54a0eb6b3cb03
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-12-10 21:09:21 +0000

    Test and implement hoover going north

--- src/hoover.js
+++ src/hoover.js
@@ -3,6 +3,18 @@
 class Hoover {
   constructor(cleaningDetails) {
     this.position = cleaningDetails.hooverPosition;
+
+    this._clean(cleaningDetails.drivingInstructions);
+  }
+
+  _clean(drivingInstructions) {
+    drivingInstructions.forEach(instruction => {
+      switch (instruction) {
+        case 'N':
+          this.position[1]++;
+          break;
+      }
+    });
   }
 }
 
--- tests/hoover.test.js
+++ tests/hoover.test.js
@@ -11,4 +11,11 @@ describe('Hoover', () => {
 
     expect(hoover.position).toStrictEqual([1, 1]);
   });
+
+  test('it can go north', () => {
+    const cleaningDetails = new CleaningDetails(5, 5, [0, 0], ['N'], []);
+    const hoover = new Hoover(cleaningDetails);
+
+    expect(hoover.position).toStrictEqual([0, 1]);
+  });
 });
